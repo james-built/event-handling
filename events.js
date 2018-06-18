406aa6ca305139da69ecfa479c8718850aeae95d
james-built <jgbuilt@gmail.com>
2018-06-18 20:41:55 +1200

    added event listeners for remaining HTML elements

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -37,7 +38,11 @@ function three() {
   three.addEventListener('mouseleave', makeWhite)
 }
 // CREATE FUNCTION four HERE
-
+function four() {
+  var four = document.getElementById('four')
+  four.addEventListener('click', makeViolet)
+  four.addEventListener('mouseleave', makeWhite)
+}
 // Changes the background color of event's target
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
@@ -49,6 +54,10 @@ function makeGreen (evt) {
 
 function makeRed (evt) {
   evt.target.style.backgroundColor = 'red'
+}
+
+function makeViolet (evt) {
+  evt.target.style.backgroundColor = 'violet'
 }
 
 function makeWhite (evt) {
